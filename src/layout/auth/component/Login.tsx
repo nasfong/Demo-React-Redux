9781fc41b4af08a5e283926341679c39155f4178
Nas Fong <fongren007@gmail.com>
2023-01-30 11:17:45 +0700

    frontend -

--- src/layout/auth/component/Login.tsx
+++ src/layout/auth/component/Login.tsx
@@ -3,7 +3,6 @@ import { useState } from 'react'
 import { Col, Form, Row } from 'react-bootstrap'
 import { useDispatch } from 'react-redux'
 import { redirect } from 'react-router-dom'
-import { socket } from '../../../App'
 
 const Login = () => {
   const dispatch = useDispatch()
